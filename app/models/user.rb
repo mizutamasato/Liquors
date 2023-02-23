92a602b02fb85a4be4dbf1c514794157df752939
EC2 Default User <ec2-user@ip-172-31-39-243.ap-northeast-1.compute.internal>
2023-02-23 09:06:27 +0000

    新規登録修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
          
   has_many :reviews, dependent: :destroy
   has_many :comments, dependent: :destroy
+  
+  def active_for_authentication?
+    super && (is_deleted == false)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  
-  devise_for :users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
+
+  devise_for :users,skip: [:registrations, :passwords], controllers: {
   sessions: 'public/sessions'
 }
+  devise_scope :users do
+    get 'user/sign_up', to: 'public/registrations#new', as: :new_user_registration
+    post 'user/sign_up', to: 'public/registrations#create', as: :user_registration
+  end
 
  # user側ルーティング
   scope module: 'public' do
@@ -11,13 +14,15 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create, :edit, :serch,:index, :show, :destroy]do
       resources :comments, only: [:create, :destroy]  #reviewsに対してcomments子となるためネストする
     end
-    
+
     get 'users/my_page' => 'users#show'
     patch 'users/update' => 'users#update'
     get 'users/edit' => 'users#edit'
+    get '/users/unsubscribe' => 'users#unsubscribe' # 退会確認画面
+    patch 'users/withdrawal' => 'users#withdrawal' # 退会の論理削除
     resources :tags, only: [:show]
   end
-  
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
