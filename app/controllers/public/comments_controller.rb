009b990552138b8df7c59efba5745d6530ecc4fb
EC2 Default User <ec2-user@ip-172-31-39-243.ap-northeast-1.compute.internal>
2023-02-25 08:41:33 +0000

    完成

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,12 +1,15 @@
 class Public::CommentsController < ApplicationController
 
   def create
-    review = Review.find(params[:review_id])
-    comment = current_user.comments.new(comment_content_params)
-    comment.review_id = review.id
-    comment.save
-    flash[:notice] = "コメントを投稿しました"
-    redirect_to review_path(review)
+    @review = Review.find(params[:review_id])
+    @comment = current_user.comments.new(comment_content_params)
+    @comment.review_id = @review.id
+    if @comment.save
+      flash[:notice] = "コメントを投稿しました"
+      redirect_to review_path(@review)
+    else
+      render 'public/reviews/show'
+    end
   end
 
   def destroy
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,8 @@
+class Users::SessionsController < Devise::SessionsController
+  
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to reviews_path, notice: 'ゲストユーザーでログインしました。'
+  end
+end
